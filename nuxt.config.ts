38f80b6cd316be1d9fd549b04cbb1414331713b6
exco-consulting <78353355+exco-consulting@users.noreply.github.com>
2023-03-03 09:20:50 +0100

    add strapi module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/strapi'],
+  strapi: {
+    // Options
+    url: process.env.STRAPI_URL || 'http://localhost:1337',
+    prefix: '/api',
+    version: 'v4',
+    cookie: {},
+    cookieName: 'strapi_jwt',
+  },
 });
